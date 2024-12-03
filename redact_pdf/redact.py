fce5b689fca537b37503b409a98df1112d67f3a6
Levy Nunes <levi@LEVY-PC.>
2024-12-03 18:01:01 -0300

    makefile, mkdocs files,

--- redact_pdf/redact.py
+++ redact_pdf/redact.py
@@ -10,12 +10,31 @@ class TextRedactor:
     """Redact a text from a pdf file
 
     Methods:
+
     - redact_text(file_path: Path, text_to_redact: str, output_file_name:Path) -> None
-    - redact_all_files_in_dir(self, base_path: Path, text_to_redact: str, output_file_suffix: str) -> None
+
+    - redact_all_files_in_dir(base_path: Path, text_to_redact: str, output_file_suffix: str) -> None
     """
 
     def redact_text(self, file_path: Path, text_to_redact: str, output_file_name: Path) -> None:
-        """Open the document and redact the text"""
+        """Open the document and redact the text
+
+        Args:
+            file_path (Path): PDF file path.
+            text_to_redact (str): Text or phrase to be redacted in every page of pdf file.
+            output_file_name (Path): PDF file name (include .pdf) to be saved after redact.
+
+        Returns:
+            None
+
+        Examples:
+            >>> from redact_pdf.redact import TextRedactor
+            >>> tr = TextRedactor()
+            >>> pdf_file = Path("path/to/input.pdf")
+            >>> TEXT_TO_REDACT = "Confidential"
+            >>> save_path = Path("path/to/output.pdf")
+            >>> tr.redact_text(file_path=pdf_file, text_to_redact=TEXT_TO_REDACT, output_file_name=save_path)
+        """
         logger.info("Opening file...")
         doc = fitz.open(file_path)
         logger.success("File Opened.")
@@ -36,6 +55,24 @@ class TextRedactor:
         logger.success("Redacted PDF saved as {}", output_file_name)
 
     def redact_all_files_in_dir(self, base_path: Path, text_to_redact: str, output_file_suffix: str) -> None:
+        """Redact all files in a local directory
+
+        Args:
+            base_path (Path): Base directory to find all *.pdf files (recursively)
+            text_to_redact (str): Text or phrase to be redacted in every page of pdf file.
+            output_file_suffix (Path): Suffix to append to original file name to be saved.
+
+        Returns:
+            None
+
+        Examples:
+            >>> from redact_pdf.redact import TextRedactor
+            >>> tr = TextRedactor()
+            >>> base_path = Path("path/to/files/")
+            >>> TEXT_TO_REDACT = "Confidential"
+            >>> suffix = "redacted"
+            >>> tr.redact_all_files_in_dir(base_path=base_path, text_to_redact=TEXT_TO_REDACT, output_file_suffix=suffix)
+        """
         for file in base_path.rglob("*.pdf"):
             file_stem = file.stem
             self.redact_text(file, text_to_redact, file.with_name(file_stem + f"_{output_file_suffix}.pdf"))
